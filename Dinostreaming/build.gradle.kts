3364729fb316a545374aaf11748a03d66e24a3d9
Danix160 <tiziotipo18@gmail.com>
2025-07-26 10:39:46 +0200

    Add files via upload

---
+++ Dinostreaming/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    language = "it"
+    description = "Plugin per dinostreaming.it"
+    authors = listOf("IlTuoNome")
+
+    status = 1 // 1 = funzionante
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://dinostreaming.it/favicon.ico"
+}
